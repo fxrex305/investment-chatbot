c1812cac0aaa9d418a866926d29ed8595adfa457
fxrex305 <flora_xie@hotmail.com>
2025-09-17 10:33:30 +1200

    Fix backend API URL to use environment variable

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
-
 export const sendMessage = async (message) => {
-  const res = await axios.post("http://localhost:8000/chat", { message });
+  const res = await axios.post(
+    process.env.REACT_APP_BACKEND_URL + "/chat",
+    { message }
+  );
   return res.data.response;
 };
